b90bce76c5d90335b37c66dfb8d7ff883f83ee04
John Heberty de Freitas <46422955+JohnHeberty@users.noreply.github.com>
2025-05-30 12:27:45 -0300

    Fix: Matriz Aerea estava errada, onde a integridade do dado tonelada não foi mantido, corrigido!

--- Pipelines/Aereo/7_Matriz_Aereo_Expandida_Zona_Pnl/MATRIZ_AEREO_EXPANDIDA_ZONA_PNL.sql
+++ Pipelines/Aereo/7_Matriz_Aereo_Expandida_Zona_Pnl/MATRIZ_AEREO_EXPANDIDA_ZONA_PNL.sql
@@ -1,12 +1,12 @@
 -- PEGANDO MATRIZ AEREA EXPANDIDA POR UTP E EXPANDINDO NOVAMENTE PARA ZONA PNL
-create materialized view matriz_cargas_codemge.matriz_aerea_final as 
+create materialized view matriz_cargas_codemge.mvw_matriz_aerea_corrigida as 
 select 
 	maeu.utp_o, 
 	maeu.utp_d, 
 	mcmicpr.zona_pnl_origem,
 	mcmicpr.zona_pnl_destino,
 	maeu.id_macroproduto_peltmg, 
-	SUM(maeu.tonelada * mcmicpr.ton_ratio) as tonelada, 
+	SUM(maeu.tonelada * mcmicpr.ton_ratio) as tonelada
 from matriz_cargas_codemge.matriz_aerea_expandida_udp maeu
 left join matriz_cargas_codemge.matriz_2019_completa_macroprod_infra_codemge_pnl_ratio mcmicpr on (
 	cast(maeu.utp_o as text) = mcmicpr.utp_o and
